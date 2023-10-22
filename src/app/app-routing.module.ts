197521678a5776cdd2bbc209e9a1f98608cc6eea
Camilodez <hernandezz_camilo@javeriana.edu.co>
2023-10-21 22:09:08 -0500

    Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// @ts-ignore
+import { LoginComponent } from './login/login.component';
+// @ts-ignore
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Ruta por defecto
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
 })
 export class AppRoutingModule { }
